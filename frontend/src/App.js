e3e338b479c7ae2ce3758d01f30cd72fd6e15696
efosdan <efosdan@yahoo.com>
2022-10-06 18:18:28 +0200

    added cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,35 @@ import { Routes, Route } from "react-router-dom";
 import AddProduct from "./components/products/AddProduct";
 import SignUp from "./components/singup-login/SignUp";
 import Login from "./components/singup-login/Login";
-import { useState, useEffect} from "react"
+import Cart from "./components/cart/Cart";
+import { useState, useEffect } from "react";
 
 function App() {
-
   const [show, setShow] = useState(true);
   const [cart, setCart] = useState([]);
-  const [cartQuantity, setCartQuantity] = useState(0)
-  
+  const [cartQuantity, setCartQuantity] = useState(0);
+
   const handleClick = (product) => {
     if (cart.indexOf(product) !== -1) return;
     product.qty = 1;
-     setCart([...cart, product]);  
-  }; 
+    setCart([...cart, product]);
+  };
 
   const handleChange = (product, d) => {
     const ind = cart.indexOf(product);
     const arr = cart;
     arr[ind].qty += d;
 
-    if (arr[ind].qty === 0)arr[ind].qty = 1;
+    if (arr[ind].qty === 0) arr[ind].qty = 1;
     setCart([...arr]);
   };
-  
-  useEffect(()=>{
-    const sumOfQuantities = cart.map(product => product.qty).reduce((previousValue, currentValue) => previousValue + currentValue, 0)
-  setCartQuantity(sumOfQuantities)
-  }, [cart])
 
+  useEffect(() => {
+    const sumOfQuantities = cart
+      .map((product) => product.qty)
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    setCartQuantity(sumOfQuantities);
+  }, [cart]);
 
   return (
     <div className="App">
@@ -42,6 +43,12 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product/new" element={<AddProduct />} />
+        <Route
+          path="/cart"
+          element={
+            <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+          }
+        />
       </Routes>
     </div>
   );
--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -1,61 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import "./CartStyles.css"
-
+import React, { useState, useEffect } from "react";
 
 function Cart({ cart, setCart, handleChange }) {
-    const [price, setPrice] = useState(0);
-    const [displayPrices, setDisplayPrices] = useState([]);
-    const [totalPrice, setTotalPrice] = useState(0)
-   
-    
-    const handleRemove = (id) => {
-        const arr = cart.filter((product) => product.id !== id);
-        setCart(arr);
-        handlePrice();
-    };
+  const [price, setPrice] = useState(0);
+  const [displayPrices, setDisplayPrices] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-    const handlePrice =() => {
-        let i = 0;
-        cart.map((product) => (i += product.qty * product.price));
-        setPrice(i);
-    };
+  const handleRemove = (id) => {
+    const arr = cart.filter((product) => product.id !== id);
+    setCart(arr);
+    handlePrice();
+  };
 
-    useEffect(() =>{
-        handlePrice();
-    }, []);
+  const handlePrice = () => {
+    let i = 0;
+    cart.map((product) => (i += product.qty * product.price));
+    setPrice(i);
+  };
 
-    useEffect(()=>{
-      const listOfPrices = cart.map(product => { return {displayPrice: product.price * product.qty} })
-      const sumOfList = cart.map(product => product.qty * product.price).reduce((previousValue, currentValue) => previousValue + currentValue, 0)
-      setDisplayPrices(listOfPrices)
-      setTotalPrice(sumOfList)
-    }, [cart])
+  useEffect(() => {
+    handlePrice();
+  }, []);
+
+  useEffect(() => {
+    const listOfPrices = cart.map((product) => {
+      return { displayPrice: product.price * product.qty };
+    });
+    const sumOfList = cart
+      .map((product) => product.qty * product.price)
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    setDisplayPrices(listOfPrices);
+    setTotalPrice(sumOfList);
+  }, [cart]);
 
   return (
     <article>
-        {cart.map((product, index) => (
-          <div className="cart_box" key={product.id}>
-            <div className="cart_img" >
+      {cart.map((product, index) => (
+        <div className="cart_box" key={product.id}>
+          <div className="cart_img">
             <img src={product.img} alt="" />
             <p>{product.name}</p>
-          </div>           
+          </div>
           <div>
             <button onClick={() => handleChange(product, 1)}>+</button>
-            <button> {product.qty} </button> 
+            <button> {product.qty} </button>
             <button onClick={() => handleChange(product, -1)}>-</button>
-          </div> 
+          </div>
           <div>
-            <span>$ {displayPrices.length !== 0 && displayPrices[index].displayPrice}</span>
+            <span>
+              ${" "}
+              {displayPrices.length !== 0 && displayPrices[index].displayPrice}
+            </span>
             <button onClick={() => handleRemove(product.id)}>Remove</button>
           </div>
-          </div>
-        ))}
-        <div className="total">
-           <span>Total Price Of Your Cart</span>
-           <span>$  {totalPrice} </span>  
-        </div>    
+        </div>
+      ))}
+      <div className="total">
+        <span>Total Price Of Your Cart</span>
+        <span>$ {totalPrice} </span>
+      </div>
     </article>
-  )
-};
+  );
+}
 
 export default Cart;
